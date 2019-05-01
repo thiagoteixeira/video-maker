3cae1d7423d9b5638585b9e54adb8db31eadb607
Thiago Teixeira <thiagoteixeiraoficial@gmail.com>
2019-05-01 20:18:46 -0300

    parsing text

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const readLine = require('readline-sync')
 
-function start() {
+const robots = {
+  userInput: require('./robots/user-input.js'),
+  text: require("./robots/text.js")
+}
+
+async function start() {
   const content = {}
-  
-  content.searchTerm = askAndReturnSearchTerm();
-  content.prefix = askAndReturnPrefix();
-
-  function askAndReturnSearchTerm() {
-    return readLine.question('Type a Wikipedia search term: ')   
-  }
-
-  function askAndReturnPrefix() {
-    const prefixes = ['Who is', "What is", 'The history of']
-    const selectedPrefixIndex = readLine.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
-    return selectedPrefixText
-  }
+ 
+  robots.userInput(content)
+  await robots.text(content)
 
   console.log(content);
 }
